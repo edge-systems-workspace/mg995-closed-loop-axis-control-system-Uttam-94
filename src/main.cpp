a564b4b79195f0881cd1cd7609dff17de3ce9bcd
Uttam-94 <uttamskk1@gmail.com>
2026-02-20 08:59:29 +0530

    module added

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
 #include <Arduino.h>
+#include <Servo.h>
+/**
+ * @file main.ino
+ * @brief Embedded Touch Detection System using TTP223
+ * @author Uttam Singh
+ * @date 2026-02-20
+ *
+ * @details
+ * Reads digital touch input from TTP223 sensor
+ * and displays structured output via Serial Monitor.
+ */
 void setup() {
 // write your initialization code here
 }
